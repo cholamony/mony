391f7252b5e1473165210e10873a7d50c18d9a21
youcholamony <abc@yahoo.com>
2013-04-04 16:30:11 +0700

    initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mony::Application.config.secret_token = 'eb9d4d12013a8a29f0c84a2632d46a85aa721b0c605d3e7eea6cd5b58f7283df14cf32f638b8bf6c84f097e7d249211b615b6478419ce1dc8a3cff18800f94b2'
